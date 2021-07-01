bb3a4fccff6bb2424bd8b3c7b69bb7d06ff1c276
Angeleena <angeleenasyse@gmail.com>
2021-06-30 23:06:49 -0700

    nav component tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -5,16 +5,17 @@ import About from '..';
 
 afterEach(cleanup);
 
-const { asFragment } = render(<About/>);
+
 
 describe('About component', () => {
     //First test
     it('renders', () => {
-        asFragment();
+        render(<About/>);
     });
     //Second test
     it('matches snapshot DOM node structure', () => {
         // render About
+        const { asFragment } = render(<About/>);
         expect(asFragment()).toMatchSnapshot();
     })
 });
